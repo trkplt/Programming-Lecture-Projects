c9343cf93dee1c299eebf95769923fb2d7e63c58
Tarik Polat <tarikpolat252@gmail.com>
2020-08-23 21:40:52 +0200

    write some method signatures for validity checks

--- Final2/src/edu/kit/informatik/wtrs/regulator/WorkTimeRegulator.java
+++ Final2/src/edu/kit/informatik/wtrs/regulator/WorkTimeRegulator.java
@@ -9,12 +9,41 @@ import java.util.TreeSet;
 public abstract class WorkTimeRegulator {
 
     private final TreeSet<WorkTime> workTimes;
+    private final TreeSet<WorkTime> assignedRestTimes;
 
     protected WorkTimeRegulator() {
         this.workTimes = new TreeSet<>();
+        this.assignedRestTimes = new TreeSet<>();
     }
 
     public abstract ErrorMessage addNewWorkTime(WorkTime workTime);
+
+    protected abstract ErrorMessage checkAverageWorkTime(WorkTime workTime);
+
+
+
+    private ErrorMessage checkPauseValidity() {
+
+    }
+
+    private void removeRestTime(WorkTime restTime) {
+
+    }
+
+    //hours: min. 11h ya da min. 12h'dan hangisi olduğunu söyleyecek
+    private ErrorMessage findAndPlaceRestTime(WorkTime workTime, int hours) {
+
+    }
+
+    private ErrorMessage checkRestTime(WorkTime workTime) {
+
+    }
+
+    protected ErrorMessage checkAverageWorkTimeIn(WorkTime workTime, int days) {
+
+    }
+
+
 
     public boolean isAssignedAt(ExactTime time) {
         for (WorkTime workTime : this.workTimes) {
